5b5f38b9adde441f3673fa3a3f62cdb87ce6939c
Matthew Bowers <matbowrs@gmail.com>
2019-04-15 18:37:35 -0400

    Quiz app setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
   </div>
 </template>
-
 <script>
 import Header from './components/Header.vue'
 import QuestionBox from './components/QuestionBox.vue'
